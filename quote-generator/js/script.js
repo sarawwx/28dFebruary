64aec060409de826fe8d2d8a602626cefd911712
Sara L <wwwsaramilk@gmail.com>
2023-02-06 23:08:25 -0300

    working

--- quote-generator/js/script.js
+++ quote-generator/js/script.js
@@ -15,5 +15,7 @@ async function getQuote(url){
 getQuote(api_url);
 
 function tweet(){
-    window.open("https://twitter.com/intent/tweet?text=Hello%20world", "Tweet Window", "width-600, height=300");
+    window.open("https://twitter.com/intent/tweet?text=" 
+    + quote.innerHTML + " -- by" + author.innerHTML,
+    "Tweet Window", "width-600, height=300");
 }
