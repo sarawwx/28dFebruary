d97574cef42df659efdfdb792555eb02da8f4780
Sara L <wwwsaramilk@gmail.com>
2023-02-06 22:21:36 -0300

    final details

--- feedback/js/script.js
+++ feedback/js/script.js
@@ -1,36 +1,34 @@
 const ratingEls = document.querySelectorAll(".rating");
-
 const btnEl = document.getElementById("btn");
 
 const containerEl = document.getElementById("container");
 
 let selectedRating = "";
 
-ratingEls.forEach((ratingEls) => {
-    ratingEls.addEventListener("click", (event) => {
-        removeActive();
-        selectedRating = 
-            event.target.innerText || event.target.parentNode.innerText;
-        event.target.classList.add("active");
-        event.target.parentNode.classList.add("active");
-    });
+ratingEls.forEach((ratingEl) => {
+  ratingEl.addEventListener("click", (event) => {
+    removeActive();
+    selectedRating =
+      event.target.innerText || event.target.parentNode.innerText;
+    event.target.classList.add("active");
+    event.target.parentNode.classList.add("active");
+  });
 });
 
 btnEl.addEventListener("click", () => {
-    if(selectedRating !== "") {
-        containerEl.innerHTML = `
+  if (selectedRating !== "") {
+    containerEl.innerHTML = `
         <strong> thank you </strong>
         <br>
         <br>
         <strong> feedback: ${selectedRating}</strong>
-        `
-    }
-})
-
-
+        <p> we'll use your feedback to improve our support </p>
+        `;
+  }
+});
 
 function removeActive() {
-    ratingEls.forEach((ratingEl) => {
-        ratingEl.classList.remove("active");
-    })
+  ratingEls.forEach((ratingEl) => {
+    ratingEl.classList.remove("active");
+  });
 }
