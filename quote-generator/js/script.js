f62a9ab6b7e4ec513cb437e1bacd53c08ef55e21
Sara L <wwwsaramilk@gmail.com>
2023-02-06 22:59:02 -0300

    function ok

--- quote-generator/js/script.js
+++ quote-generator/js/script.js
@@ -1,7 +1,15 @@
 const api_url =  "https://api.quotable.io"
 
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+
+
 async function getQuote(url){
     const response = await fetch(url);
     var data = await response.json();
-    console.log(data);
+    
+    quote.innerHTML = data.content;
+    author.innerHTML = data.author;
 };
+
+getQuote(apu_url);
