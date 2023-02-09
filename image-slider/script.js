e5f1a0dd2ec0f7914d68dad2f942adb03e3cc9ad
Sara L <wwwsaramilk@gmail.com>
2023-02-08 21:02:59 -0300

    js start

---
+++ image-slider/script.js
@@ -0,0 +1,16 @@
+const carousel = document.querySelector(".carousel");
+
+
+let isDragStart = false;
+
+const dragStart = () => {
+    isDragStart = true;
+}
+
+const dragging = (e) => {
+    if(!isDragStart) return;
+    carousel.scrollLeft = e.pageX;
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
