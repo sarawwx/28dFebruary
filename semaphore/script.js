52d6668b767d53f35a5e264c97f31aee2e8bc6c2
Sara L <wwwsaramilk@gmail.com>
2023-02-13 20:53:36 -0300

    function change color ok

--- semaphore/script.js
+++ semaphore/script.js
@@ -1,14 +1,32 @@
 const img = document.getElementById('img');
 const buttons = document.getElementById('buttons');
 
+let colorIndex = 0;
+let intervalId = null;
+
 const trafficLight = (event) => {
-    turnOn.red();
+    stopAutomatic();
+    turnOn[event.target.id]();
+}
+
+const nextIndex = () => colorIndex = colorIndex < 2 ? ++colorIndex : 0;
+
+const changeColor = () => {
+    const colors = [ 'red', 'yellow', 'green']
+    const color = colors[ colorIndex ];
+    turnOn[color]();
+    nextIndex();
+}
+
+const stopAutomatic = () => {
+    clearInterval (intervalId);
 }
 
 const turnOn = {
     'red': () => img.src = './img/red.png',
     'yellow': () => img.src = './img/yellow.png',
     'green': () => img.src = './img/green.png',
+    'automatic': () => intervalId = setInterval( changeColor, 1000 )
 }
 
 buttons.addEventListener('click', trafficLight);
