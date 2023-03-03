96bdc0648d82274abb985e7ecd9cc3cecd219200
Sara L <wwwsaramilk@gmail.com>
2023-03-03 12:37:14 -0300

    content array

--- memory-game/app.js
+++ memory-game/app.js
@@ -6,8 +6,20 @@ document.addEventListener('DOMContentLoaded', () => {
             img: 'img/cash.jpg'
         },
         {
+            name: 'cash',
+            img: 'img/cash.jpg'
+        },
+        {
             name: 'clock',
             img: 'img/clock.jpg'
+        },
+        {
+            name: 'clock',
+            img: 'img/clock.jpg'
+        },
+        {
+            name: 'coffee',
+            img: 'img/coffee.jpg'
         },
         {
             name: 'coffee',
@@ -18,8 +30,20 @@ document.addEventListener('DOMContentLoaded', () => {
             img: 'img/cookie.jpg'
         },
         {
+            name: 'cookie',
+            img: 'img/cookie.jpg'
+        },
+        {
             name: 'pizza',
             img: 'img/pizza.jpg'
+        },
+        {
+            name: 'pizza',
+            img: 'img/pizza.jpg'
+        },
+        {
+            name: 'icecream',
+            img: 'img/icecream.jpg'
         },
         {
             name: 'icecream',
