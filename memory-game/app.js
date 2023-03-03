f87ddcafa3f984bd7a70d2fa5bd1e556eb64ff42
Sara L <wwwsaramilk@gmail.com>
2023-03-03 12:35:14 -0300

    scripting

---
+++ memory-game/app.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const cardArray = [
+        {
+            name: 'cash',
+            img: 'img/cash.jpg'
+        },
+        {
+            name: 'clock',
+            img: 'img/clock.jpg'
+        },
+        {
+            name: 'coffee',
+            img: 'img/coffee.jpg'
+        },
+        {
+            name: 'cookie',
+            img: 'img/cookie.jpg'
+        },
+        {
+            name: 'pizza',
+            img: 'img/pizza.jpg'
+        },
+        {
+            name: 'icecream',
+            img: 'img/icecream.jpg'
+        }
+    ]
+})
